d5cd83ccf473df4911ad1e3fbe2ed1d17d6115c0
Garrett Tallman <84149469+garrett-tallman@users.noreply.github.com>
2024-11-18 08:42:16 -0600

    modernization

---
+++ script.js
@@ -0,0 +1,24 @@
+// Typewriter Effect
+const nameText = "Garrett Tallman";
+const titleText = "Machine Learning Product Manager";
+let nameIndex = 0;
+let titleIndex = 0;
+
+function typeEffect(id, text, speed) {
+    const element = document.getElementById(id);
+    if (id === "name" && nameIndex < text.length) {
+        element.innerHTML += text.charAt(nameIndex);
+        nameIndex++;
+        setTimeout(() => typeEffect(id, text, speed), speed);
+    } else if (id === "title" && titleIndex < text.length) {
+        element.innerHTML += text.charAt(titleIndex);
+        titleIndex++;
+        setTimeout(() => typeEffect(id, text, speed), speed);
+    }
+}
+
+// Start Typing Animation
+window.onload = () => {
+    typeEffect("name", nameText, 100); // Type name at 100ms speed
+    setTimeout(() => typeEffect("title", titleText, 100), nameText.length * 100 + 500); // Start title after name
+};

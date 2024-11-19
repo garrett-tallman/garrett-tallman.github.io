f2dac14546484eb21518454b43c8c3de84aa412b
Garrett Tallman <84149469+garrett-tallman@users.noreply.github.com>
2024-11-19 09:05:46 -0600

    style changes and about page

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-// Typewriter Effect
-const nameText = "Garrett Tallman";
-const titleText = "Product Manager";
-let nameIndex = 0;
-let titleIndex = 0;
-
 function typeEffect(id, text, speed, callback) {
     const element = document.getElementById(id);
-    if (id === "name" && nameIndex < text.length) {
-        element.innerHTML += text.charAt(nameIndex);
-        nameIndex++;
-        setTimeout(() => typeEffect(id, text, speed, callback), speed);
-    } else if (id === "title" && titleIndex < text.length) {
-        element.innerHTML += text.charAt(titleIndex);
-        titleIndex++;
-        setTimeout(() => typeEffect(id, text, speed, callback), speed);
-    } else if (callback) {
-        callback(); // Trigger the callback after the typing animation
+    let index = 0;
+
+    function typeCharacter() {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeCharacter, speed);
+        } else if (callback) {
+            callback(); // Trigger callback when typing is complete
+        }
     }
+
+    typeCharacter();
 }
 
-// Reveal Projects with Motion
 function revealProjects() {
     const linksContainer = document.getElementById("links-container");
     linksContainer.classList.remove("hidden");
     linksContainer.classList.add("visible");
 }
 
-// Start Typing Animation
-window.onload = () => {
-    typeEffect("name", nameText, 85, () => {
-        typeEffect("title", titleText, 85, revealProjects); // Reveal projects after title typing
-    });
-};
+function applyEffectsForPage() {
+    const currentPage = window.location.pathname;
+    typeEffect("name", "Garrett Tallman", 85, revealProjects);
+}
+
+// Ensure animations start when the page is fully loaded
+window.onload = applyEffectsForPage;
